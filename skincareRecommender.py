fbe68640f37392e7714677e0abe021eab58be87a
nazifaaa22 <nullah@terpmail.umd.edu>
2024-04-08 16:46:17 -0400

    start

---
+++ skincareRecommender.py
@@ -0,0 +1,12 @@
+import skincare_dataset 
+
+class skincare:
+
+def userQuestions():
+    """asks the user questions about their skin 
+    """
+    skintype = input("What is your skinstype?")
+    targetConcern = input("What is your skin concern?")
+
+ 
+
