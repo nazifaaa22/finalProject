5300c83bb95f30e89e08f650e20566af65dd022d
skebede4 <149550755+skebede4@users.noreply.github.com>
2024-04-08 22:56:01 -0400

    Update skincareRecommender.py

--- skincareRecommender.py
+++ skincareRecommender.py
@@ -1,8 +1,14 @@
 import skincare_dataset 
 
 class skincare:
+    """A class for determining which skincare products work best for user.
+
+    
     """
-    """
+    def __init__(self):
+        """Initializes a skincare object.
+        
+        """
 
 def userQuestions():
     """asks the user questions about their skin 
