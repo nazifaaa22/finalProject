49b5b3de4971b450ab2c83cf4e0c9bd2cfedb0a6
nazifaaa22 <nullah@terpmail.umd.edu>
2024-04-22 22:11:15 -0400

    Update skincareRecommender.py

--- skincareRecommender.py
+++ skincareRecommender.py
@@ -1,4 +1,6 @@
+import sys
 import skincare_dataset 
+import pandas as pd
 
 class skincare:
     """A class for determining which skincare products work best for user.
@@ -26,6 +28,36 @@ def userQuestions():
     skintype = input("What is your skinstype?")
     targetConcern = input("What is your skin concern?")
     budget = input("What is your budget?")
+class SkincareRecommender:
+    """_summary_
+    """
+    def __init__(self, dataset_path):
+        self.products_df = pd.read_csv(dataset_path)
+
+    def userQuestions():
+        """asks the user questions about their skin 
+        """
+        skintype = input("What is your skinstype?")
+        targetConcern = input("What is your skin concern?")
+
+class Moisturizer:
+    def __init__():
+class Cleanser: 
+    def (): 
+class Treatment: 
+    def ():
+        if targetConcern = acne 
+            return = salicylic acid  
+        elif targetConcern = anti aging 
+            return = retinol 
+class Face_Mask: 
+    def (): 
+class Eye_Cream:
+    def (): 
+class Sun_Protection: 
+    def (): 
+
+
 
  
 
