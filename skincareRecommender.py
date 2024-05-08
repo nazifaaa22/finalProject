47ee6627f224a7bebe6bc98d67337d70ba565953
nazifaaa22 <nullah@terpmail.umd.edu>
2024-05-08 19:59:55 -0400

    Update skincareRecommender.py

--- skincareRecommender.py
+++ skincareRecommender.py
@@ -9,14 +9,8 @@ class Skincare:
     Methods:
     
     """
-<<<<<<< HEAD
     def __init__(self, path):
         """Initializes a skincare object.
-=======
-    def __init__(self):
-        """ Initializes a Skincare object.
-
->>>>>>> 62fa8fbb4a23a8b8eb8a89cc1a3554d4ddfc64e8
         Args:
         Returns:
         """
@@ -29,22 +23,10 @@ class Skincare:
             None
         """
         skintype = input("What is your skinstype?")
-        targetConcern = input("What is your skin concern?")
+        targetConcern = input("From the list provided, what is your top skin concern? Dryness, Acne, Dull SKin, Sun Protection")
         budget = input("What is your budget?")
 
-<<<<<<< HEAD
         return skintype, targetConcern, budget 
-=======
-def userQuestions():
-    """ Asks the user questions about their skin, including their skin type, their skincare concern, and their budget.
-
-    Returns:
-        None
-    """
-    skintype = input("What is your skinstype?")
-    targetConcern = input("What is your skin concern?")
-    budget = input("What is your budget?")
->>>>>>> 62fa8fbb4a23a8b8eb8a89cc1a3554d4ddfc64e8
 
 class SkincareRecommender:
     """_summary_
@@ -64,6 +46,14 @@ class Moisturizer:
     def __init__():
         """ Initializes a Moisturizer object. 
         """
+        moisturizer = ("What is your skintype? (Choose 1-5, 1: Combo, 2: Dry, 3: Normal, 4: Oily, 5: Sensitive)")
+        skin_types = ["Combo", "Dry", "Normal", "Oily", "Sensitive"]
+        budget = ("What is your budget?")
+
+        for skin_type in skin_types:
+            print(skin_type) 
+
+        return  
 class Cleanser: 
     def __init__(): 
         """ Initializes a Cleanser object.  
@@ -80,22 +70,34 @@ class Face_Mask:
     def __init__():
         """ Initializes a Face Mask object.
         """
+        face_mask = ("What is your skintype? (Choose 1-4, 1: Combo, 2: Dry, 3: Normal, 4: Oily, 5: Sensitive)")
+
+        if "skincare_dataset" == 
+
 class Eye_Cream:
     def __init__(): 
         """ Initializes an Eye Cream object.
         """
+        eye_cream = 
 class Sun_Protection:
     def __init__(): 
         """ Initializes a Sun Protection object. 
         """
 
-def main (): 
+def main (path): 
     dataset_path = 'skincare_dataset.csv'
     recommender = SkincareRecommender(dataset_path)
     skintype, targetConcern = userQuestions()
 
+    skincarerecommender = []
+    with open(path, 'r') as file:
+        newfile = file.readlines()
+
+    return "The best moisturizer is" brand "with" price
+
+
 if __name__ == "__main__":
-    main() 
+    main("skincare_dataset.txt") 
 
 
 
